e45dcb643154f433ca91972f174b4e5ae91c4c5d
Kyle Colantonio <k@yle.sh>
2024-02-27 18:47:58 -0500

    feat(aws): Removing Netmaker and AWS resources

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,16 +1,12 @@
 locals {
   # General
-  aws_account_id    = data.aws_caller_identity.current.account_id
-  aws_account_alias = data.aws_iam_account_alias.current.account_alias
-  aws_region        = data.aws_region.current.name
   tags = merge(var.tags, {
-    Terraform = "true",
-    VPC       = var.vpc_name
+    Terraform  = "true",
+    Repository = "IAreKyleW00t/k2net"
   })
 
-  # CDN
-  cdn_domain       = "s3.${var.cloudflare_zone}"
-  s3_origin_id     = "s3-${aws_s3_bucket.cdn.id}"
+  # B2
   b2_domain        = "b2.${var.cloudflare_zone}"
-  b2_public_bucket = "${var.vpc_name}-public"
+  b2_public_bucket = "k2net-public"
+  b2_backup_bucket = "k2net-backups"
 }
